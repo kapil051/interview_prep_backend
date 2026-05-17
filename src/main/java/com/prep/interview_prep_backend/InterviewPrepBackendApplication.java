5af5d19551de1cf44fdf301580890bff34f2f001
QL-Kapil-Kant <kapil.kant@quokkalabs.com>
2026-05-17 12:32:14 +0530

    (feat): Add project files and .gitignore

---
+++ src/main/java/com/prep/interview_prep_backend/InterviewPrepBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prep.interview_prep_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPrepBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPrepBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prep/interview_prep_backend/InterviewPrepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prep.interview_prep_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPrepBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
