319381c13db1b4fb336aff38e4d134064498f608
QL-Kapil-Kant <kapil.kant@quokkalabs.com>
2026-05-30 17:31:56 +0530

    (fix): add enum for the blind 75 module tracking

---
+++ src/main/java/com/interviewprep/backend/module/questions/blind75/enums/Pattern.java
@@ -0,0 +1,7 @@
+package com.interviewprep.backend.module.questions.blind75.enums;
+
+public enum Pattern {
+    HASHMAP, HASHSET, TWO_POINTERS, SLIDING_WINDOW, FAST_SLOW_POINTERS,
+    BINARY_SEARCH, DFS, BFS, DYNAMIC_PROGRAMMING, BACKTRACKING, GREEDY,
+    STACK, HEAP, TRIE
+}
---
+++ src/main/java/com/interviewprep/backend/module/questions/blind75/enums/ProgressStatus.java
@@ -0,0 +1,5 @@
+package com.interviewprep.backend.module.questions.blind75.enums;
+
+public enum ProgressStatus {
+    SOLVED, ATTEMPTED, TODO
+}
